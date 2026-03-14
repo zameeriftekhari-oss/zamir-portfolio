69c387ac4a66069947b224d4ec180a2727511596
zameeriftekhari-oss <zameer.iftekhari@gmail.com>
2026-03-15 02:06:00 +0800

    Update splitText.ts

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -1,7 +1,7 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollSmoother } from "gsap/ScrollSmoother";
-import { SplitText } from "gsap/SplitText";
+import SplitText from "gsap/SplitText";
 
 interface ParaElement extends HTMLElement {
   anim?: gsap.core.Animation;
